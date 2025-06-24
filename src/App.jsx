3b4cfe8f9ab230c4e1628066f283eb1a52249b28
Seth Berrier <berriers@uwstout.edu>
2025-06-24 07:19:52 -0500

    Basic example with Box as its own component

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
+
+import Box from './components/Box.jsx'
+
+export default function App() {
+  return (
+    // A base canvas to hold our scene
+    <Canvas>
+      {/* Some lights to brighten up the world */}
+      <ambientLight intensity={Math.PI / 2} />
+      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
+      <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+
+      {/* Two Boxes */}
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
+
+      {/* Allow orbit controls */}
+      <OrbitControls />
+    </Canvas>
+  )
+}
---
+++ src/components/Box.jsx
@@ -0,0 +1,45 @@
+import React, { useRef, useState } from 'react'
+import { useFrame } from '@react-three/fiber'
+
+export default function Box(props) {
+  // This reference gives us direct access to the THREE.Mesh object
+  const ref = useRef()
+
+  // State and callbacks for hovering
+  const [hovered, setHovered] = useState(false)
+  const onHovered = (e) => {
+    e.stopPropagation()
+    document.body.style.cursor = 'pointer'
+    setHovered(true)
+  }
+
+  const onNotHovered = (e) => {
+    document.body.style.cursor = 'default'
+    setHovered(false)
+  }
+
+  // State and callbacks for clicking
+  const [clicked, setClicked] = useState(false)
+  const onClicked = (e) => {
+    setClicked(!clicked)
+  }
+
+  // Subscribe this component to the render-loop, rotate the mesh every frame
+  useFrame((state, delta) => {
+    ref.current.rotation.x += delta
+  })
+
+  // Return the view, these are regular Three.js elements expressed in JSX
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      scale={clicked ? 1.5 : 1}
+      onClick={onClicked}
+      onPointerOver={onHovered}
+      onPointerOut={onNotHovered}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  )
+}
